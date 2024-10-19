a59b2001dc20bbc927f6722ddc9a43a4b677e7a5
Moose-222 <prfletcher22@gmail.com>
2024-10-19 09:03:39 +0000

    Updated server.js test

--- server.js
+++ server.js
@@ -42,3 +42,7 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'Test route is working!' });
+});
+
